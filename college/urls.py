f5576ab75a407bb8bc0db0daa65b0f1f52a535f0
Rahul Kushwaha <55339596+RahulKushwaha762@users.noreply.github.com>
2020-02-20 16:48:16 +0530

    Added index

--- college/urls.py
+++ college/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('upload/',views.upload),
     path('upload/usingupload/',views.uploadimage),
     path('upload/usinglink/',views.link),
-    path('upload/delete/<int:dl>/',views.delete),
+    path('index/upload/delete/<int:dl>/',views.delete),
     path('newcategory/',views.newcat)
 ]
